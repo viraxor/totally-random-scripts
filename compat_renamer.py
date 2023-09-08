1618d2e08e2551f681e6b84f0c3f257fc46ce5bd
viraxor <55741238+viraxor@users.noreply.github.com>
2023-09-08 10:28:58 +0200

    Update compat_renamer.py

--- compat_renamer.py
+++ compat_renamer.py
@@ -3,3 +3,6 @@ import os
 
 for file in glob.glob("./*.compat.*"):
     os.rename(file, file.replace(".compat", ""))
+    
+for file in glob.glob("./*.*"):
+    os.rename(file, file.replace(" ", "_"))
